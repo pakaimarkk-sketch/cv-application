8364bb00a8969df2b024222f9e05ce30acecb37d
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-08-29 12:28:56 +0200

    feat: state management for app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { GeneralInfo } from "./components/GeneralInfo";
 import { EducationInfo } from "./components/EducationInfo";
 import { ExperienceInfo } from "./components/ExperienceInfo";
+import { CVPreview } from "./components/CVPreview";
 
 export function App() {
   const [currentStep, setCurrentStep] = useState(0);
@@ -17,7 +18,8 @@ export function App() {
   const [educationInfo, setEducationInfo] = useState({
     school: "",
     title: "",
-    date: "",
+    from: "",
+    until: "",
   });
 
   const [experienceInfo, setExperienceInfo] = useState({
@@ -28,5 +30,67 @@ export function App() {
     until: "",
   });
 
-  return <main></main>;
+  function handleNext() {
+    setCurrentStep((step) => step + 1);
+  }
+
+  function handlePrevious() {
+    setCurrentStep((step) => step - 1);
+  }
+
+  function handleChange(event, setter) {
+    const { name, value } = event.target;
+
+    setter((currentInfo) => ({
+      ...currentInfo,
+      [name]: value,
+    }));
+  }
+
+  function handleSave() {
+    setCurrentStep(3);
+  }
+
+  function handleEdit() {
+    setCurrentStep(0);
+  }
+
+  return (
+    <main>
+      {currentStep === 0 && (
+        <GeneralInfo
+          data={generalInfo}
+          onChange={(event) => handleChange(event, setGeneralInfo)}
+          onNext={handleNext}
+        />
+      )}
+
+      {currentStep === 1 && (
+        <EducationInfo
+          data={educationInfo}
+          onChange={(event) => handleChange(event, setEducationInfo)}
+          onPrevious={handlePrevious}
+          onNext={handleNext}
+        />
+      )}
+
+      {currentStep === 2 && (
+        <ExperienceInfo
+          data={experienceInfo}
+          onChange={(event) => handleChange(event, setExperienceInfo)}
+          onPrevious={handlePrevious}
+          onSave={handleSave}
+        />
+      )}
+
+      {currentStep === 3 && (
+        <CVPreview
+          generalInfo={generalInfo}
+          educationInfo={educationInfo}
+          experienceInfo={experienceInfo}
+          onEdit={handleEdit}
+        />
+      )}
+    </main>
+  );
 }
